106adf6d4ee2339e89385bd8fb537d33d03516d9
Pat Walls <patrickswalls@gmail.com>
2018-09-30 00:02:58 -0400

    add sentry

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Gratiboard
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    Raven.configure do |config|
+      config.dsn = 'https://80cceebaeb2a4af8bfceb5fc436702ef:c8a328605eae48b5b6c17bfd312773b3@sentry.io/1291295'
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
